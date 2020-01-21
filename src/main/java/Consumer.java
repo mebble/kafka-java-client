2d01f41d0e4246e9da7edea5f22f046087fa9c31
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-01-21 15:07:12 +0530

    initial commit

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,14 @@
+package kafka_java_clients_api;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class Consumer {
+    Consumer() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("COmpiled!");
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+package kafka_java_clients_api;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("yes");
+    }
+}
