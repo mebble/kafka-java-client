d16fabe98a747671ca491b04e56a7fc43894aed0
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-01-22 18:50:49 +0530

    restructure myconsumer class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,18 @@
 package kafka_java_clients_api;
 
-import java.util.Arrays;
-import java.time.Duration;
-
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-
 public class Main {
     public static void main(String[] args) throws java.io.IOException {
-        String consumerPropertiesPath = System.getProperty("user.dir") + "/src/main/resources/consumer.properties";
-        String producerPropertiesPath = System.getProperty("user.dir") + "/src/main/resources/producer.properties";
+        String resourcesPath = System.getProperty("user.dir") + "/src/main/resources/";
 
-        Consumer consumer = ConsumerFactory.getConsumer(consumerPropertiesPath);
-        consumer.subscribe(Arrays.asList("test-topic"));
+        MyConsumer consumer1 = new MyConsumer(resourcesPath + "consumer1.properties");
+        MyConsumer consumer2 = new MyConsumer(resourcesPath + "consumer2.properties");
 
-        // run the kafka consumer
-        System.out.println("Running the kafka consumer...");
-        while (true) {
-            System.out.println("polling...");
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
-            for (ConsumerRecord<String, String> record : records) {
-                System.out.println("----- Record: -----");
-                System.out.println("key: " + record.key());
-                System.out.println("value: " + record.value());
-                System.out.println("partition: " + record.partition());
-                System.out.println("offset: " + record.offset());
-                System.out.println("-------------------");
-            }
-            consumer.commitAsync();
-        }
-//        consumer.close();
+        consumer1.subscribe(new String[] { "test-topic" });
+        consumer2.subscribe(new String[] { "test-topic" });
+
+        Thread consumer1Thread = new Thread(consumer1);
+        Thread consumer2Thread = new Thread(consumer2);
+        consumer1Thread.start();
+        consumer2Thread.start();
     }
 }
---
+++ src/main/java/MyConsumer.java
@@ -0,0 +1,49 @@
+package kafka_java_clients_api;
+
+import java.util.Properties;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.time.Duration;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.errors.WakeupException;
+
+public class MyConsumer implements Runnable {
+    private static int consumerCount = 0;
+
+    private int consumerId;
+    private Consumer consumer;
+
+    MyConsumer(String propertiesPath) throws IOException {
+        FileReader file = new FileReader(propertiesPath);
+        Properties props = new Properties();
+        props.load(file);
+
+        this.consumer = new KafkaConsumer<>(props);
+        this.consumerId = consumerCount;
+
+        consumerCount++;
+    }
+
+    public void subscribe(String[] topics) {
+        this.consumer.subscribe(Arrays.asList(topics));
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("Running kafka consumer %d...\n", this.consumerId);
+        while (true) {
+            System.out.printf("consumer %d polling...\n", this.consumerId);
+            ConsumerRecords<String, String> records = this.consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("----- %d received: -----\n", this.consumerId);
+                System.out.printf("key: %s,\tvalue: %s,\tpartition: %d,\toffset: %d\n", record.key(), record.value(), record.partition(), record.offset());
+            }
+            this.consumer.commitAsync();
+        }
+    }
+}
